c853b4bdc9c79ce57d9c2bc3970b484388c6cc37
Edison Martins <martinsegmj@gmail.com>
2017-02-16 00:32:33 -0200

    Commit Inicial

---
+++ socialbooks-client/src/main/java/com/algaworks/socialbooks/client/domain/Livro.java
@@ -0,0 +1,99 @@
+package com.algaworks.socialbooks.client.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+public class Livro {
+	
+	@JsonInclude(Include.NON_NULL)
+	private Long id;
+	
+	@JsonInclude(Include.NON_NULL)
+	private String nome;
+	
+	@JsonInclude(Include.NON_NULL)
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date publicacao;
+	
+	@JsonInclude(Include.NON_NULL)
+	private String editora;
+	
+	@JsonInclude(Include.NON_NULL)
+	private String resumo;
+	
+	@JsonInclude(Include.NON_EMPTY)
+	private List<Comentario> comentarios;
+	
+		@JsonInclude(Include.NON_NULL)
+	private Autor autor;
+	public Livro(String nome) {
+		this.nome = nome;
+	}
+	
+	public Livro(){
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getPublicacao() {
+		return publicacao;
+	}
+
+	public void setPublicacao(Date publicacao) {
+		this.publicacao = publicacao;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public String getResumo() {
+		return resumo;
+	}
+
+	public void setResumo(String resumo) {
+		this.resumo = resumo;
+	}
+
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+	
+
+}
